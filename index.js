e851c9a2a95e7f3812467315e065c4669121cafc
gconnell29 <gconnelljr29@gmail.com>
2020-02-18 22:50:36 -0500

    implemented filter method

--- index.js
+++ index.js
@@ -207,8 +207,7 @@ function getFullNames(runners) {
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
 function firstNamesAllCaps(runners) {
-  let firstNames = [];
-  runners.map(firstNames.push(runners.first_name.toUpperCase()));
+  let firstNames = runners.map(runner => runner.first_name.toUpperCase());
   return firstNames;
 }
 
@@ -225,8 +224,8 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, size) {
+  let matchedSize = runners.filter(element => size === element.shirt_size);
 }
 
 /**
